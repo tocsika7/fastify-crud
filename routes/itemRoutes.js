bdf2e530b58f3dc15386c37400e96d2f55c36b74
tocsika7 <tocsamanatee@gmail.com>
2021-06-19 19:50:19 +0200

    Created routes for getting all items and getting an item by id

---
+++ routes/itemRoutes.js
@@ -0,0 +1,18 @@
+const items = require("../data/Items");
+
+function itemRoutes(fastify, options, done) {
+  fastify.get("/items", (req, reply) => {
+    reply.send(items);
+  });
+
+  fastify.get("/items/:id", (req, reply) => {
+    const { id } = req.params;
+
+    const item = items.find((item) => item.id === id);
+    reply.send(item);
+  });
+
+  done();
+}
+
+module.exports = itemRoutes;
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const fastify = require("fastify")({ logger: true });
 const PORT = 5000;
 
+fastify.register(require("./routes/itemRoutes"));
+
 const start = async () => {
   try {
     await fastify.listen(PORT);
